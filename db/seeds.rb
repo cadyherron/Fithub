19cd4f729cf02d990dcf3eca646c547523eefbb3
Julia Herron <julia.herron@gmail.com>
2016-02-25 10:32:39 -0800

    user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.delete_all
+
+
+# change this number to scale amount of seeds
+MULTIPLIER = 1
+
+
+
+puts "Creating users..."
+
+def create_user
+  User.create(
+    first_name: Faker::Name.first_name, 
+    last_name: Faker::Name.last_name, 
+    email: Faker::Internet.email, 
+    password_digest: "$2a$10$.MSTCUKj.7tpap8LswJXa.AeTlsa9Qmh0TWTcYJjqZd8bhMokgPbO"
+    )
+end
+
+(MULTIPLIER*10).times do
+  create_user
+end
