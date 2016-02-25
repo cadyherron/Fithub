1b3be724cbdd7047e606c6d55a5a3b7d92d3ada4
Julia Herron <julia.herron@gmail.com>
2016-02-25 14:29:34 -0800

    login before meal search

--- spec/features/user_creates_a_meal_spec.rb
+++ spec/features/user_creates_a_meal_spec.rb
@@ -2,13 +2,20 @@ require 'rails_helper'
 
 feature 'User creates a meal' do
 
+  let(:user) { create(:user) }
+
+  before do
+    visit root_path
+    fill_in("Email", with: user.email)
+    fill_in("Password", with: user.password)
+    click_on("Log in")
+  end
+
   scenario 'they search for meal items' do
     visit new_meal_path
-    fill_in 'Food search', with: 'Apple'
-    save_and_open_page
+    find('#food_search').set('apple')
     click_button "Search"
-
-    expect(page).to have_content 'Apple'
+    expect(page).to have_content 'Apple - 1 cup slices'
   end
 
 end
