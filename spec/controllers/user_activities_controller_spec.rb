468374fa9e535edd82539f35e4a0fab18639ad99
Kit Langton <kitlangton@gmail.com>
2016-02-25 21:28:11 -0500

    spec fix

--- spec/controllers/user_activities_controller_spec.rb
+++ spec/controllers/user_activities_controller_spec.rb
@@ -1,16 +1,15 @@
 require 'rails_helper'
 
-
-
 describe UserActivitiesController do
 
   describe 'with_logged_in_user' do
     let(:user){ create(:user) }
     let(:user_activity){ create(:user_activity, user: user) }
 
-  before :each do
-    request.cookies["auth_token"] = user.auth_token
+    before :each do
+      request.cookies["auth_token"] = user.auth_token
+    end
+
   end
 
 end
-
