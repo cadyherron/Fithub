9ec228d02e59e6f318d686806c7ada08e21b588a
Deepa Kackar <dkackar99@yahoo.com>
2016-02-25 16:57:18 -0800

    Added totals for calories and minutes

--- app/models/user_activity.rb
+++ app/models/user_activity.rb
@@ -1,4 +1,5 @@
 class UserActivity < ActiveRecord::Base
+  before_create :fill_in_nill
 
   belongs_to :activity
   belongs_to :user
@@ -11,8 +12,38 @@ class UserActivity < ActiveRecord::Base
   validates :user, presence: true
   validates :activity, presence: true
 
-  def self.user_activties_for_today(user)
+  def self.user_activities_for_today(user)
     today = Time.new.strftime('%Y-%m-%d')
     user.user_activities.where("created_at.strftime('%Y-%m-%d')" == today)
+  end 
+
+  def self.user_calories_for_today(user,scope="today")
+    if scope == "today"
+      activities = UserActivity.user_activities_for_today(user)
+    else
+      activities =user.user_activities
+    end  
+    activities.inject(0) {|sum, activity| sum += activity.calories }
+
+  end
+
+  def self.user_minutes_for_today(user,scope="today")
+    if scope == "today"
+      activities = UserActivity.user_activities_for_today(user)
+    else
+      activities =user.user_activities
+    end  
+    activities.inject(0) {|sum, activity| sum += activity.duration_minutes }
+    
+  end
+
+  def fill_in_nill
+    if self.calories.nil?
+       self.calories = 0
+    end
+    
+    if self.duration_minutes.nil?
+       self.duration_minutes = 0
+    end
   end 
 end
